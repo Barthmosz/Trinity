c514e4a106409938ba06c3820befd431382a49a1
Barthmosz <barthmosz@gmail.com>
2023-03-08 21:42:47 -0300

    feat: create GetAllAsync in StaticPersistence

---
+++ Trinity.Persistence/Contracts/IStaticPersistence.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Trinity.Persistence.Contracts
+{
+  public interface IStaticPersistence<D> where D : class
+  {
+    Task<IEnumerable<D>> GetAllAsync();
+  }
+}
---
+++ Trinity.Persistence/Persistence/StaticPersistence.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Trinity.Persistence.Contexts;
+using Trinity.Persistence.Contracts;
+
+namespace Trinity.Persistence.Persistence
+{
+  public class StaticPersistence<D> : IStaticPersistence<D> where D : class
+  {
+    protected IMongoDbContext MongoDbContext;
+    protected IMongoCollection<D> MongoCollection;
+
+    public StaticPersistence(IMongoDbContext mongoDbContext)
+    {
+      this.MongoDbContext = mongoDbContext;
+      this.MongoCollection = this.MongoDbContext.GetCollection<D>();
+    }
+
+    public virtual async Task<IEnumerable<D>> GetAllAsync()
+    {
+      return await this.MongoCollection.Find(Builders<D>.Filter.Empty).ToListAsync();
+    }
+  }
+}
