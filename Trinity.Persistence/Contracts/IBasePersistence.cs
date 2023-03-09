cfc4a0d4ac8be6c513698e5218468ea2eedce0e8
Barthmosz <barthmosz@gmail.com>
2023-03-08 21:43:00 -0300

    feat: create Add in BasePersistence

---
+++ Trinity.Persistence/Contracts/IBasePersistence.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Trinity.Persistence.Contracts
+{
+  public interface IBasePersistence<D> : IStaticPersistence<D> where D : class
+  {
+    Task<bool> Add(D entity);
+  }
+}
---
+++ Trinity.Persistence/Persistence/BasePersistence.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Trinity.Persistence.Contexts;
+using Trinity.Persistence.Contracts;
+
+namespace Trinity.Persistence.Persistence
+{
+  public class BasePersistence<D> : StaticPersistence<D>, IBasePersistence<D> where D : class
+  {
+    public BasePersistence(IMongoDbContext mongoDbContext) : base(mongoDbContext) { }
+
+    public async Task<bool> Add(D entity)
+    {
+      await this.MongoCollection.InsertOneAsync(entity);
+      return true;
+    }
+  }
+}
