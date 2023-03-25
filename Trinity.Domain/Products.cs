e45d6bd18eefac1138a464115779cb77ad67a567
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-25 17:06:51 -0300

    feat: add base Products domain

---
+++ Trinity.Domain/Products.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Trinity.Domain
+{
+    public class Products
+    {
+        [BsonElement]
+        [BsonRepresentation(BsonType.String)]
+        public string Name { get; set; } = string.Empty;
+
+        [BsonElement]
+        [BsonRepresentation(BsonType.Int32)]
+        public int Quantity { get; set; }
+
+        [BsonElement]
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Price { get; set; }
+    }
+}
