c6366d30773e051ade301c86041bf98667ec26fb
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-25 17:08:03 -0300

    feat: create ProductsService GetProductsAsync

---
+++ Trinity.Application/Contracts/IProductsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Trinity.Domain;
+
+namespace Trinity.Application.Contracts
+{
+    public interface IProductsService
+    {
+        Task<IEnumerable<Products>> GetProductsAsync();
+    }
+}
---
+++ Trinity.Application/Services/ProductsService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Trinity.Application.Contracts;
+using Trinity.Domain;
+using Trinity.Persistence.Contracts;
+
+namespace Trinity.Application.Services
+{
+    public class ProductsService : IProductsService
+    {
+        private readonly IStaticPersistence<Products> productStaticPersistence;
+
+        public ProductsService(IStaticPersistence<Products> productStaticPersistence)
+        {
+            this.productStaticPersistence = productStaticPersistence;
+        }
+
+        public async Task<IEnumerable<Products>> GetProductsAsync()
+        {
+            return await this.productStaticPersistence.GetAllAsync();
+        }
+    }
+}
