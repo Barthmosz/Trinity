793b04245b135e64a94d1f33747b8e0f3e29dd94
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-25 17:08:29 -0300

    feat: create ProductsController GetProductsAsync

---
+++ Trinity.API/Controllers/Products/ProductsController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Net;
+using System.Threading.Tasks;
+using Trinity.Application.Contracts;
+
+namespace Trinity.API.Controllers.Product
+{
+    [ApiController]
+    [Route("api")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductsService productService;
+
+        public ProductsController(IProductsService productService)
+        {
+            this.productService = productService;
+        }
+
+        [HttpGet("/v1/products")]
+        public async Task<IActionResult> GetProductsAsync()
+        {
+            try
+            {
+                var products = await this.productService.GetProductsAsync();
+                return StatusCode((int)HttpStatusCode.OK, new { data = products });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new { error = ex.Message });
+            }
+        }
+    }
+}
