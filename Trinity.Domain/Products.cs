75caa8ed75e49b100563c44f5c8901dbc595c1ab
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-26 15:22:37 -0300

    refactor: add new properties to Products domain

--- Trinity.Domain/Products.cs
+++ Trinity.Domain/Products.cs
@@ -11,11 +11,23 @@ namespace Trinity.Domain
         public string Name { get; set; } = string.Empty;
 
         [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string Description { get; set; } = string.Empty;
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.String)]
+        public string Image { get; set; } = string.Empty;
+
+        [BsonRequired]
         [BsonRepresentation(BsonType.Int32)]
         public int Quantity { get; set; }
 
         [BsonRequired]
         [BsonRepresentation(BsonType.Decimal128)]
         public decimal Price { get; set; }
+
+        [BsonRequired]
+        [BsonRepresentation(BsonType.Decimal128)]
+        public decimal Discount { get; set; }
     }
 }
