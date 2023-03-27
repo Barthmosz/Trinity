000a8b454a09ce23d4dde20fee0670541b684988
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-26 21:19:31 -0300

    feat: create ProductsDTO

---
+++ Trinity.Application/DTOs/ProductsDTO.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Trinity.Application.DTOs
+{
+    public class ProductsDTO
+    {
+        [JsonPropertyName("id")]
+        public string Id { get; set; } = string.Empty;
+
+        [JsonPropertyName("name")]
+        [JsonRequired]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("description")]
+        [JsonRequired]
+        public string Description { get; set; } = string.Empty;
+
+        [JsonPropertyName("image")]
+        public string Image { get; set; } = string.Empty;
+
+        [JsonPropertyName("quantity")]
+        [JsonRequired]
+        public int Quantity { get; set; }
+
+        [JsonPropertyName("price")]
+        [JsonRequired]
+        public decimal Price { get; set; }
+
+        [JsonPropertyName("discount")]
+        public decimal Discount { get; set; }
+    }
+}
