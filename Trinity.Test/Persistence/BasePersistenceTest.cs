1e2908dcb09dc52445cad45b50d5f26c24fe850d
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-03-31 14:05:18 -0300

    test: ensure Add persistence returns true

---
+++ Trinity.Test/Persistence/BasePersistenceTest.cs
@@ -0,0 +1,39 @@
+﻿using MongoDB.Driver;
+using Moq;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trinity.Domain.Base;
+using Trinity.Persistence.Contexts;
+using Trinity.Persistence.Persistence;
+using Trinity.Test.Context;
+
+namespace Trinity.Test.Persistence
+{
+    public class BasePersistenceTest
+    {
+        private IMongoDbContext mongoDbContext;
+        BasePersistence<Document> basePersistence;
+        Document document;
+        List<Document> documents;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.document = new();
+            this.documents = Enumerable.Repeat(this.document, 500).ToList();
+
+            this.mongoDbContext = new MongoDbContextMock(new Mock<IMongoDatabase>());
+            ((MongoDbContextMock)this.mongoDbContext).InitCollection(this.documents);
+            this.basePersistence = new(this.mongoDbContext);
+        }
+
+        [Test]
+        public async Task AddOk()
+        {
+            bool result = await this.basePersistence.Add(this.document);
+            Assert.That(result, Is.EqualTo(true));
+        }
+    }
+}
