34c39ec6e910a11e641e4030a595cd529a06931e
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-02 19:22:26 -0300

    feat: create signup route

---
+++ Trinity.API/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Threading.Tasks;
+using Trinity.API.ViewModels;
+using Trinity.Application.Contracts;
+using Trinity.Application.DTOs.Products;
+using Trinity.Application.DTOs.Users;
+
+namespace Trinity.API.Controllers
+{
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPost("v1/accounts/signup")]
+        public async Task<IActionResult> SignUp([FromBody] UsersInput user, [FromServices] IUsersService usersService)
+        {
+            try
+            {
+                UsersOutput userCreated = await usersService.AddUserAsync(user);
+                return StatusCode((int)HttpStatusCode.OK, new ResultViewModel<UsersOutput>(userCreated));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new ResultViewModel<IEnumerable<ProductsOutput>>(ex.Message));
+            }
+        }
+    }
+}
