87e0bb769f40231d4c7479887fd3608c0a46e886
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-02 19:12:28 -0300

    feat: create UsersService AddUserAsync

---
+++ Trinity.Application/Contracts/IUsersService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Trinity.Application.DTOs.Users;
+
+namespace Trinity.Application.Contracts
+{
+    public interface IUsersService
+    {
+        Task<UsersOutput> AddUserAsync(UsersInput user);
+    }
+}
