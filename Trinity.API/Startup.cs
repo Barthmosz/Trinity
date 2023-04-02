72b6cf6dc7a28cd68a35f32d7b3e3917ea3cebce
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-02 19:14:39 -0300

    chore: add authentication, authorization and user scoped

--- Trinity.API/Startup.cs
+++ Trinity.API/Startup.cs
@@ -1,8 +1,11 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using Trinity.Application.Contracts;
 using Trinity.Application.Mapping;
 using Trinity.Application.Services;
@@ -35,15 +38,37 @@ namespace Trinity.API
                 options.Name = this.Configuration.GetSection("DatabaseSettings:Name").Value;
             });
 
+            byte[] key = Encoding.ASCII.GetBytes(ConfigurationApp.JwtKey);
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddTransient<TokenService>();
+
             services.AddAutoMapper(typeof(DomainToMappingProfile));
 
             services.AddScoped<IConnectionConfig, ConnectionConfig>();
             services.AddScoped<IMongoDbContext, MongoDbContext>();
 
             services.AddScoped<IStaticPersistence<Products>, StaticPersistence<Products>>();
+            services.AddScoped<IStaticPersistence<Users>, StaticPersistence<Users>>();
             services.AddScoped<IBasePersistence<Products>, BasePersistence<Products>>();
+            services.AddScoped<IBasePersistence<Users>, BasePersistence<Users>>();
 
             services.AddScoped<IProductsService, ProductsService>();
+            services.AddScoped<IUsersService, UsersService>();
         }
 
         public void Configure(IApplicationBuilder application, IWebHostEnvironment environment)
@@ -57,6 +82,8 @@ namespace Trinity.API
             application.UseHttpsRedirection();
             application.UseRouting();
             application.UseCors(options => options.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+            application.UseAuthentication();
+            application.UseAuthorization();
             application.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
