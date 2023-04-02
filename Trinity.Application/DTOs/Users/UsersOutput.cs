29d76c7d1abfe82cfc9425d687bcdb8f46d6e8ca
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-02 19:11:50 -0300

    feat: create UsersOutput DTO

---
+++ Trinity.Application/DTOs/Users/UsersOutput.cs
@@ -0,0 +1,11 @@
+﻿namespace Trinity.Application.DTOs.Users
+{
+    public class UsersOutput
+    {
+        public string Id { get; set; } = string.Empty;
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Email { get; set; } = string.Empty;
+    }
+}
