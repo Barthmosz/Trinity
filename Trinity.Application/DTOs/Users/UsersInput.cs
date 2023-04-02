0e5dd485d30a58eab7e26a224f4800ec008331bb
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-02 19:11:41 -0300

    feat: create UsersInput DTO

---
+++ Trinity.Application/DTOs/Users/UsersInput.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Trinity.Application.DTOs.Users
+{
+    public class UsersInput
+    {
+        [JsonPropertyName("name")]
+        [JsonRequired]
+        public string Name { get; set; } = string.Empty;
+
+        [JsonPropertyName("email")]
+        [JsonRequired]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [JsonPropertyName("password")]
+        [JsonRequired]
+        public string Password { get; set; } = string.Empty;
+    }
+}
