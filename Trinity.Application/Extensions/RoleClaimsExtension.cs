24af7bde47866f55f0f0ecc46000e8ea673847a2
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-03 15:33:53 -0300

    feat: implement RoleClaimsExtension to get Account claims

---
+++ Trinity.Application/Extensions/RoleClaimsExtension.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using Trinity.Domain.Accounts;
+
+namespace Trinity.Application.Extensions
+{
+    public static class RoleClaimsExtension
+    {
+        [Authorize]
+        public static IEnumerable<Claim> GetClaims(this Accounts account)
+        {
+            List<Claim> result = new()
+            {
+                new(ClaimTypes.Name, account.Email)
+            };
+
+            result.AddRange(account.Roles.Select(role => new Claim(ClaimTypes.Role, role)));
+            return result;
+        }
+    }
+}
