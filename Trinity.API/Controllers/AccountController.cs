1412fda925e3424d84e7bcd7ca5a074470b5d0d7
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-03 16:11:09 -0300

    feat: implement signin route

--- Trinity.API/Controllers/AccountController.cs
+++ Trinity.API/Controllers/AccountController.cs
@@ -14,12 +14,26 @@ namespace Trinity.API.Controllers
     public class AccountController : ControllerBase
     {
         [HttpPost("v1/accounts/signup")]
-        public async Task<IActionResult> SignUp([FromBody] UsersInput user, [FromServices] IUsersService usersService)
+        public async Task<IActionResult> SignUp([FromBody] AccountsInput account, [FromServices] IAccountsService accountsService)
         {
             try
             {
-                UsersOutput userCreated = await usersService.AddUserAsync(user);
-                return StatusCode((int)HttpStatusCode.OK, new ResultViewModel<UsersOutput>(userCreated));
+                AccountsOutput userCreated = await accountsService.SignUpAsync(account);
+                return StatusCode((int)HttpStatusCode.OK, new ResultViewModel<AccountsOutput>(userCreated));
+            }
+            catch (Exception ex)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, new ResultViewModel<IEnumerable<ProductsOutput>>(ex.Message));
+            }
+        }
+
+        [HttpPost("v1/accounts/signin")]
+        public async Task<IActionResult> SignIn([FromBody] AccountsInput account, [FromServices] IAccountsService accountsService)
+        {
+            try
+            {
+                string token = await accountsService.SignInAsync(account);
+                return StatusCode((int)HttpStatusCode.OK, new ResultViewModel<string>(token, null));
             }
             catch (Exception ex)
             {
