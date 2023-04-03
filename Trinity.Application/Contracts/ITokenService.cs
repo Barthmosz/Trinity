824e0fdf4e8670cf4c6b6d627ede52c4f8ed358c
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-03 15:31:59 -0300

    feat: implement TokenService generate

---
+++ Trinity.Application/Contracts/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using Trinity.Domain.Accounts;
+
+namespace Trinity.Application.Contracts
+{
+    public interface ITokenService
+    {
+        string GenerateToken(Accounts user);
+    }
+}
---
+++ Trinity.Application/Services/TokenService.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Trinity.Application.Contracts;
+using Trinity.Application.Extensions;
+using Trinity.Domain.Accounts;
+
+namespace Trinity.Application.Services
+{
+    public class TokenService : ITokenService
+    {
+        public string GenerateToken(Accounts account)
+        {
+            JwtSecurityTokenHandler tokenHandler = new();
+            byte[] key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+            IEnumerable<Claim> claims = account.GetClaims();
+            SecurityTokenDescriptor tokenDescriptor = new()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.UtcNow.AddHours(8),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            SecurityToken token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
