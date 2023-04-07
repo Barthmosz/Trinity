8b4c4bf9b78a5d8b373de96a777f7f49926d48d4
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-07 12:41:26 -0300

    test: ensure SignIn should return Ok if email and password are valid

--- Trinity.Test/API/Controllers/Accounts/AccountsControllerTest.cs
+++ Trinity.Test/API/Controllers/Accounts/AccountsControllerTest.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Moq;
 using NUnit.Framework;
+using SecureIdentity.Password;
 using System;
 using System.Net;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using Trinity.API.Controllers.Accounts;
 using Trinity.Application.Contracts;
 using Trinity.Application.DTOs.Users;
 using Trinity.Application.Services;
+using Trinity.Application.Wrappers;
 using Trinity.Domain.Entities.Accounts;
 using Trinity.Persistence.Contracts;
 
@@ -18,6 +20,7 @@ namespace Trinity.Test.API.Controllers.Account
     {
         private readonly Mock<IStaticPersistence<Accounts>> accountsStaticPersistence = new();
         private readonly Mock<IBasePersistence<Accounts>> accountsBasePersistence = new();
+        private readonly Mock<IPasswordHasherWrapper> passwordHasher = new();
         private readonly Mock<ITokenService> tokenService = new();
         private readonly Mock<IMapper> mapper = new();
 
@@ -25,6 +28,7 @@ namespace Trinity.Test.API.Controllers.Account
         private AccountsController accountsController;
 
         private AccountsSignUpInput accountsSignUpInput;
+        private AccountsSignInInput accountsSignInInput;
         private AccountsOutput accountsOutput;
 
         private Accounts? account;
@@ -36,6 +40,11 @@ namespace Trinity.Test.API.Controllers.Account
             this.accountsSignUpInput = new()
             {
                 Name = "any_name",
+                Email = "any_email@mail.com",
+                Password = "any_password"
+            };
+            this.accountsSignInInput = new()
+            {
                 Email = "any_email@mail.com",
                 Password = "any_password"
             };
@@ -56,8 +65,9 @@ namespace Trinity.Test.API.Controllers.Account
             this.mapper.Setup(m => m.Map<AccountsOutput>(this.account)).Returns(this.accountsOutput);
             this.accountsStaticPersistence.Setup(p => p.GetByEmailAsync(It.IsAny<string>())).Returns(Task.FromResult(this.account));
             this.accountsBasePersistence.Setup(p => p.AddAsync(It.IsAny<Accounts>())).Returns(Task.FromResult(true));
+            this.passwordHasher.Setup(p => p.Verify(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<short>(), It.IsAny<int>(), It.IsAny<char>(), It.IsAny<string>())).Returns(true);
 
-            this.accountsService = new AccountsService(this.accountsStaticPersistence.Object, this.accountsBasePersistence.Object, this.tokenService.Object, this.mapper.Object);
+            this.accountsService = new AccountsService(this.accountsStaticPersistence.Object, this.accountsBasePersistence.Object, this.passwordHasher.Object, this.tokenService.Object, this.mapper.Object);
             this.accountsController = new(this.accountsService);
         }
 
@@ -97,6 +107,16 @@ namespace Trinity.Test.API.Controllers.Account
 
             ObjectResult? result = await this.accountsController.SignUp(this.accountsSignUpInput) as ObjectResult;
             Assert.That(result!.StatusCode, Is.EqualTo((int)HttpStatusCode.InternalServerError));
+        }
+        #endregion
+
+        #region SignIn
+        [Test]
+        public async Task SignIn_Should_Return_Ok_If_Email_And_Password_Are_Valid()
+        {
+            this.accountsStaticPersistence.Setup(p => p.GetByEmailAsync(It.IsAny<string>())).Returns(Task.FromResult(this.accountExists)!);
+            ObjectResult? result = await this.accountsController.SignIn(this.accountsSignInInput) as ObjectResult;
+            Assert.That(result!.StatusCode, Is.EqualTo((int)HttpStatusCode.OK));
         }
         #endregion
     }
