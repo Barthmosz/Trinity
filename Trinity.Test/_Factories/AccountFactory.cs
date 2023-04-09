b88fa38aff837978594440ccc682cb1998bc220e
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-09 17:17:44 -0300

    feat: create AccountSignUpInput factory

--- Trinity.Test/_Factories/AccountFactory.cs
+++ Trinity.Test/_Factories/AccountFactory.cs
@@ -4,6 +4,16 @@ namespace Trinity.Test.Factories
 {
     public static class AccountFactory
     {
+        public static AccountSignUpInput MakeAccountSignUpInput()
+        {
+            return new AccountSignUpInput()
+            {
+                Name = "any_name",
+                Email = "any_email@mail.com",
+                Password = "any_password"
+            };
+        }
+
         public static AccountSignInInput MakeAccountSignInInput()
         {
             return new AccountSignInInput()
