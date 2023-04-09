6f4783d4a66bfde0fa05f9e7e9de4bfaa2db32ce
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-09 00:49:37 -0300

    feat: add Hash method to PasswordHasherWrapper

--- Trinity.Application/Wrappers/IPasswordHasherWrapper.cs
+++ Trinity.Application/Wrappers/IPasswordHasherWrapper.cs
@@ -2,6 +2,7 @@
 {
     public interface IPasswordHasherWrapper
     {
+        string Hash(string password, short saltSize = 16, short keySize = 32, int iterations = 10000, char splitChar = '.', string privateKey = "");
         bool Verify(string hash, string password, short keySize = 32, int iterations = 10000, char splitChar = '.', string privateKey = "");
     }
 }
--- Trinity.Application/Wrappers/PasswordHasherWrapper.cs
+++ Trinity.Application/Wrappers/PasswordHasherWrapper.cs
@@ -6,6 +6,11 @@ namespace Trinity.Application.Wrappers
     [ExcludeFromCodeCoverage]
     public class PasswordHasherWrapper : IPasswordHasherWrapper
     {
+        public string Hash(string password, short saltSize = 16, short keySize = 32, int iterations = 10000, char splitChar = '.', string privateKey = "")
+        {
+            return PasswordHasher.Hash(password, saltSize, keySize, iterations, splitChar, privateKey);
+        }
+
         public bool Verify(string hash, string password, short keySize = 32, int iterations = 10000, char splitChar = '.', string privateKey = "")
         {
             return PasswordHasher.Verify(hash, password, keySize, iterations, splitChar, privateKey);
