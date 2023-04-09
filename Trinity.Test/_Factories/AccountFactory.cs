c5a93f2b8fd77714ac9a581e20ca4001c91633e8
Barthmosz <126964783+Barthmosz@users.noreply.github.com>
2023-04-09 17:17:00 -0300

    feat: create AccountSignInInput factory

---
+++ Trinity.Test/_Factories/AccountFactory.cs
@@ -0,0 +1,16 @@
+﻿using Trinity.Application.DTOs.Account;
+
+namespace Trinity.Test.Factories
+{
+    public static class AccountFactory
+    {
+        public static AccountSignInInput MakeAccountSignInInput()
+        {
+            return new AccountSignInInput()
+            {
+                Email = "any_email@mail.com",
+                Password = "any_password"
+            };
+        }
+    }
+}
